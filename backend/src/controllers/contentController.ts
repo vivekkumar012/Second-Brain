3ec47739486a01186c58dbecc0cc88aa45615356
Vivek Kumar <vh118854@gmail.com>
2025-08-12 00:59:40 +0530

    update some codes

---
+++ backend/src/controllers/contentController.ts
@@ -0,0 +1,15 @@
+export const createContent = async (req, res) => {
+
+}
+
+export const getContent = async (req, res) => {
+
+}
+
+export const deleteContent = async (req, res) => {
+
+}
+
+export const linkShare = async (req, res) => {}
+
+export const linkShareId = async (req, res) => {}
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { login, signup } from '../controllers/userController.js';
+import { createContent, deleteContent, getContent, linkShare, linkShareId } from '../controllers/contentController.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
+userRouter.post("/content", createContent);
+userRouter.get("/content", getContent);
+userRouter.delete("/content", deleteContent);
+userRouter.post("/brain/share", linkShare);
+userRouter.get("brain/:shareLink", linkShareId);
 
 export default userRouter;
