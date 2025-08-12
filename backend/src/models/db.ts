06a42af52187d38b5216801529c2b0e4dce3c019
Vivek Kumar <vh118854@gmail.com>
2025-08-12 13:09:14 +0530

    model added

--- backend/src/models/db.ts
+++ backend/src/models/db.ts
@@ -13,3 +13,23 @@ const userSchema = new mongoose.Schema({
 })
 
 export const userModel = mongoose.model("User", userSchema);
+
+const contentSchema = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    link: {
+        type: String
+    },
+    tags: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag"
+    }],
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+})
+
+export const contentModel = mongoose.model("Content", contentSchema);
