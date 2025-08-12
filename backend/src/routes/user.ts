7e1bd9396dbd10ed6e161dd6d9b6f131ea8d058c
Vivek Kumar <vh118854@gmail.com>
2025-08-12 13:45:29 +0530

    user router

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import { login, signup } from '../controllers/userController.js';
 import { createContent, deleteContent, getContent, linkShare, linkShareId } from '../controllers/contentController.js';
+import { userMiddleware } from '../middlewares/userMiddleware.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
-userRouter.post("/content", createContent);
-userRouter.get("/content", getContent);
-userRouter.delete("/content", deleteContent);
+userRouter.post("/content", userMiddleware, createContent);
+userRouter.get("/content", userMiddleware, getContent);
+userRouter.delete("/content", userMiddleware, deleteContent);
 userRouter.post("/brain/share", linkShare);
 userRouter.get("brain/:shareLink", linkShareId);
 
